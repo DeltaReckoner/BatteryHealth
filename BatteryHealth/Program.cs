77087a3d56c1b7fa78d48dc0aaab1caf75399e77
DeltaReckoner <deltareckoner@gmail.com>
2025-04-16 19:47:53 -0400

    Add files for project

---
+++ BatteryHealth/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BatteryHealth;
+using System.Text;
+
+BatteryInformation batteryInfo = BatteryInfo.GetBatteryInformation();
+StringBuilder sb = new StringBuilder("Battery Information:\n");
+sb.AppendLine("Current Capacity: " + batteryInfo.CurrentCapacity + " mWh");
+sb.AppendLine("Designed Max Capacity: " + batteryInfo.DesignedMaxCapacity + " mWh");
+sb.AppendLine("Full Charge Capacity: " + batteryInfo.FullChargeCapacity + " mWh");
+sb.AppendLine("Voltage: " + batteryInfo.Voltage / 1000.0 + " V");
+sb.AppendLine("Discharge Rate: " + Math.Abs(batteryInfo.DischargeRate) + " mW");
+sb.AppendLine("Battery State: " + batteryInfo.BatteryState);
+sb.AppendLine("Battery Health: " + batteryInfo.BatteryHealthPercentage + "%");
+
+Console.WriteLine(sb.ToString());
+Console.ReadKey();
